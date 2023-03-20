c627219b76a49284a9d698cd8a1904e63281d73a
jishuya <jishuya3015@gmail.com>
2023-03-21 08:26:09 +0900

    JS:Wheel event

--- main.js
+++ main.js
@@ -11,7 +11,6 @@ document.addEventListener('scroll', () => {
   }
 });
 
-
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
@@ -32,37 +31,31 @@ navbarToggleBtn.addEventListener('click', () => {
 
 // Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector('.home__contact');
-homeContactBtn.addEventListener('click', ()=>{
-  scrollIntoView('#contact')
-})
-
-function scrollIntoView(selector) {
-  const scrollTo = document.querySelector(selector);
-  scrollTo.scrollIntoView({behavior : 'smooth'})
-}
+homeContactBtn.addEventListener('click', () => {
+  scrollIntoView('#contact');
+});
 
 // Make home slowly fade to transparent as the window scrolls down
-const home = document.querySelector('.home__container')
+const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
-document.addEventListener('scroll', ()=>{
-  home.style.opacity = 1 - (window.scrollY / homeHeight)
-})
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
 
-
-// Show 'arrow up' button when scrolling down
+// Show "arrow up" button when scrolling down
 const arrowUp = document.querySelector('.arrow-up');
-document.addEventListener('scroll', ()=>{
-  if(window.scrollY > homeHeight /2){
-    arrowUp.classList.add('visible')
+document.addEventListener('scroll', () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.classList.add('visible');
   } else {
     arrowUp.classList.remove('visible');
   }
-})
+});
 
 // Handle click on the "arrow up" button
-arrowUp.addEventListener('click', ()=>{
-  scrollIntoView('#home')
-})
+arrowUp.addEventListener('click', () => {
+  scrollIntoView('#home');
+});
 
 // Projects
 const workBtnContainer = document.querySelector('.work__categories');
@@ -76,8 +69,9 @@ workBtnContainer.addEventListener('click', (e) => {
 
   // Remove selection from the previous item and select the new one
   const active = document.querySelector('.category__btn.selected');
-  active.classList.remove('selected');
-  const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode
+  if (active != null) {
+    active.classList.remove('selected');
+  }
   e.target.classList.add('selected');
 
   projectContainer.classList.add('anim-out');
@@ -94,11 +88,80 @@ workBtnContainer.addEventListener('click', (e) => {
   }, 300);
 });
 
-document.addEventListener('scroll', () => {
-    console.log('Y:', window.screenY)
-  if (window.scrollY > navbarHeight) {
-    navbar.classList.add('navbar--dark');
-  } else {
-    navbar.classList.remove('navbar--dark');
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+}
+
+// Intersection Observer API는 브라우저에서 뷰포트 내에서 요소들이 교차되는지를 감지하는 기능을 제공
+// Intersection Observer API의 콜백 함수에는 두 개의 매개변수(entries와 observer)가 전달됩니다. 
+// entries 매개변수는 관찰 중인 요소들에 대한 정보를 담은 배열입니다. 배열의 각 요소는 관찰 중인 하나의 요소에 대한 정보를 담고 있습니다. 
+// observer 매개변수는 IntersectionObserver 객체 자체를 참조합니다.
+
+// 1. 모든 섹션 요소들과 메뉴아이템들을 가지고 온다
+// 2. IntersectionObserver를 이용해서 모든 섹션들을 관찰한다
+// 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다
+const sectionIds = [
+  '#home',
+  '#about',
+  '#skills',
+  '#work',
+  '#testimonials', 
+  '#contact',
+];
+
+// section의 dom 요소 가져오기 ex) id="navbar"
+const sections = sectionIds.map((id) => document.querySelector(id));
+// ex) navbar__menu의 dom요소 가져오기 data-link="#home"
+const navItems = sectionIds.map((id) =>
+  document.querySelector(`[data-link="${id}"]`)
+);
+
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];  // data-link="#home"
+function selectNavItem(selected) {
+  selectedNavItem.classList.remove('active');
+  selectedNavItem = selected;
+  selectedNavItem.classList.add('active');
+}
+
+const observerOptions = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.3,
+};
+
+const observerCallback = (entries, observer) => {
+  entries.forEach((entry) => {
+    // 빠져나갈때: 관찰중이 요소가 뷰포트내에 보임 
+    // && 관찰 중인 요소가 뷰포트 내에서 전혀안보이는것 뺴고 - 페이지만들어지고 나간것들이 걸려
+    if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+      console.log('y');
+      // ex) skills index = 2
+      const index = sectionIds.indexOf(`#${entry.target.id}`);
+      // 스크롤링이 아래로 되어서 페이지가 올라올 때, 다음인덱스 = 기존인덱스 + 1
+      if (entry.boundingClientRect.y < 0) {
+        selectedNavIndex = index + 1;
+      // 스크롤링이 위로 되어서 페이지가 올라올 때, 다음인덱스 = 기존인덱스 + 1
+      } else {
+        selectedNavIndex = index - 1;
+      }
+    }
+  });
+};
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach((section) => observer.observe(section));
+
+window.addEventListener('wheel', () => {
+  if (window.scrollY === 0) {
+    selectedNavIndex = 0;
+  } else if (
+    // (0.0) + 현재 윈도우창 높이  === documentPage 전체높이
+    window.scrollY + window.innerHeight === document.body.clientHeight
+  ) {
+    selectedNavIndex = navItems.length - 1;
   }
+  // navbar__menu의 dom요소 navItems[1] = data-link="#about"
+  selectNavItem(navItems[selectedNavIndex]);
 });
