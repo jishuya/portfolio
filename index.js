8a184568155d575ce260f45334697b0f63e41368
jishuya <jishuya3015@gmail.com>
2023-06-27 05:39:43 +0900

    HTML-header

--- index.js
+++
@@ -1,9 +0,0 @@
-const express  = require('express');
-
-const app = express();
-
-app.get('/', (req, res)=>{
-    res.send('💙Docker TEST page💚')
-});
-
-app.listen(8080, ()=> console.log('server is running💨'))
--- test.js
+++
@@ -1,5 +0,0 @@
-let item = '😀'
-let obj = { name: 'shu', age: 20};
-
-module.export();
-item.export();
