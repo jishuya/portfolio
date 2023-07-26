33206030bfac4a7f7804107c58fe1b03c3b540ae
jishuya <jishuya3015@gmail.com>
2023-07-27 08:43:40 +0900

    JS-Observe__basic

---
+++ src/active_menu.js
@@ -0,0 +1,28 @@
+// 구현 계획
+// 1. 모든 섹션 요소들과 메뉴 아이템을 가지고 온다.
+// 2. IntersectionObserver를 사용해서 모든 섹션들을 관찰해야한다.
+// 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화시킨다.
+
+// 보여지는 섹션:
+// - 다수의 섹션이 동시에 보여진다면, 가장 첫번째 섹션을 선택
+// - 마지막 contact 섹션이 보여진다면, 그러면 가장 마지막을 선택 
+
+
+const sectionIds = [ '#home', '#about', '#skills', '#work', '#testimonials', '#contact' ]; 
+const section = sectionIds.map((id) => document.querySelector(id));
+// console.log(section)
+
+const navItems = sectionIds.map((id)=> document.querySelector(`[href="${id}"]`));
+// console.log(navItems)
+
+const options = {};
+const observer = new IntersectionObserver(observerCallback, options);
+section.forEach((section)=> observer.observe(section));
+
+function observerCallback(entries){
+    entries.forEach((entry)=>{
+        console.log(entry.target);
+        console.log(entry.isIntersecting);
+        console.log(entry.isIntersectionRatio);
+    })
+}
