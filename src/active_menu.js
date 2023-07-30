934872791e59a1aebec804a592a6047d472a34f3
jishuya <jishuya3015@gmail.com>
2023-07-31 05:54:50 +0900

    JS-Observer2

--- src/active_menu.js
+++ src/active_menu.js
@@ -4,37 +4,70 @@
 // 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화시킨다.
 
 // 보여지는 섹션:
-// - 다수의 섹션이 동시에 보여진다면, 가장 첫번째 섹션을 선택
-// - 마지막 contact 섹션이 보여진다면, 그러면 가장 마지막을 선택 
+// - 다수의 섹션이 동시에 보여진다면, 가장 첫번째 섹션을 선택 → findFirstIntersecting()
+// - 마지막 contact 섹션이 보여진다면, 그러면 가장 마지막을 선택 →  selectLastOne
+
+// 해당 메뉴아이템 활성화 : selectNavItem()
 
 
-const sectionIds = [ '#home', '#about', '#skills', '#work', '#testimonials', '#contact' ]; 
-const section = sectionIds.map((id) => document.querySelector(id));
-// 보여지는 섹션
-const visibleSections = sectionIds.map(()=> false)
-
-const navItems = sectionIds.map((id)=> document.querySelector(`[href="${id}"]`));
-// console.log(navItems)
-
-const options = {};
-const observer = new IntersectionObserver(observerCallback, options);
-section.forEach((section)=> observer.observe(section));
-
-function observerCallback(entries){
+const sectionIds = [
+    '#home',
+    '#about',
+    '#skills',
+    '#work',
+    '#testimonial',
+    '#contact',
+  ];
+  // 실제 section
+  const sections = sectionIds.map((id) => document.querySelector(id));
+  // 실제 nav
+  const navItems = sectionIds.map((id) => document.querySelector(`[href="${id}"]`));
+  // 현재 섹션들의 관찰여부 (true, false)
+  const visibleSections = sectionIds.map(() => false);
+  // 현재 활성화된 nav
+  let activeNavItem = navItems[0];
+  
+  const options = {
+    rootMargin: '-30% 0px 0px 0px',
+    threshold: [0, 0.98],
+  };
+  const observer = new IntersectionObserver(observerCallback, options);
+  sections.forEach((section) => observer.observe(section));
+  
+  function observerCallback(entries) {
     let selectLastOne;
-    entries.forEach((entry)=>{
-        // console.log(entry.target);
-        // console.log(entry.isIntersecting);
-        // console.log(entry.isIntersectionRatio);
-        console.log('entry.target.id: ', entry.target.id)
-        const index = sectionIds.indexOf(`#${entry.target.id}`);
-        visibleSections[index] = entry.intersecting;
-        selectLastOne = 
-            index === sectionIds.length - 1 &&
-            entry.intersecting &&
-            entry.intersectionRatio >= 0.99;
+    entries.forEach((entry) => {
+      // 현재 보여지고 있는 섹션의 index 찾기
+      const index = sectionIds.indexOf(`#${entry.target.id}`);
+      visibleSections[index] = entry.isIntersecting;
+      selectLastOne =
+        index === sectionIds.length - 1 &&
+        entry.isIntersecting &&
+        entry.intersectionRatio >= 0.95;
     });
-    console.log('visibleSections: ', visibleSections);
-    console.log('selectLastOne: ', selectLastOne);
-}
+  
+    // navIndex: 현재 관찰중인 section으로 인덱스 찾기
+    const navIndex = selectLastOne
+      ? sectionIds.length - 1
+      : findFirstIntersecting(visibleSections);
 
+    selectNavItem(navIndex);
+  }
+  
+  
+// visibleSections 중에서 첫번째로 true인 것의 index 반환
+  function findFirstIntersecting(intersections) {
+    console.log('intersections: ', intersections)
+    const index = intersections.indexOf(true);
+    return index >= 0 ? index : 0;
+  }
+  
+  // 현재관찰 중인 section의 navIndx 스타일링
+  function selectNavItem(index) {
+    const navItem = navItems[index];
+    if (!navItem) return;
+    activeNavItem.classList.remove('active');
+    activeNavItem = navItem;
+    activeNavItem.classList.add('active');
+  }
+  
